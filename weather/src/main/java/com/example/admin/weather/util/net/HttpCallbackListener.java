d58d5b9ea49e46769e230750d276c61754b526de
LiangWenchao <1096278578@qq.com>
2016-05-24 14:37:04 +0800

    删除weather

--- weather/src/main/java/com/example/admin/weather/util/net/HttpCallbackListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.weather.util.net;
-
-/**
- * Created by admin on 2016/4/27.
- */
-public interface HttpCallbackListener {
-    void onFinish(String response);
-    void onError(Exception e);
-}
